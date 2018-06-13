6bdd5e0e44333fc8977b27cb371dee9df4152fe7
Inatsu, Yoshinari | COED <ts-yoshinari.inatsu@rakuten.com>
2018-06-13 15:25:28 +0900

    type for effects

--- src/domain/middleware/effects/pokemon.js
+++ src/domain/middleware/effects/pokemon.js
@@ -1,34 +1,36 @@
 // @flow
-import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
-
+import type {
+  UpdatePokemonDetailProcess
+} from 'domain/actions';
+import { call, put } from 'redux-saga/effects'
 import { getLogger } from 'logger';
 import { getPokemonList, getPokemonDetailByName } from 'domain/network/pokemon';
 import { updatePokemonListAction, updatePokemonDetailAction, updatePokemonListFetchingAction, updatePokemonDetailFetchingAction } from 'domain/actions';
 
 const logger = getLogger('domain/middleware/network/pokemon');
 
-export function* updatePokemonList() {
-    logger.debug('List from network');
-    yield put(updatePokemonListFetchingAction(true));
-    try {
-        const list = yield call(getPokemonList);
-        yield put(updatePokemonListAction(list));
-        yield put(updatePokemonListFetchingAction(false));
-    } catch (error) {
-        logger.error('Fetch updatePokemonList failure.', error);
-        yield put(updatePokemonListFetchingAction(false));
-    }
+export function* updatePokemonList(): Generator<any, void, any> {
+  logger.debug('List from network');
+  yield put(updatePokemonListFetchingAction(true));
+  try {
+    const list = yield call(getPokemonList);
+    yield put(updatePokemonListAction(list));
+    yield put(updatePokemonListFetchingAction(false));
+  } catch (error) {
+    logger.error('Fetch updatePokemonList failure.', error);
+    yield put(updatePokemonListFetchingAction(false));
+  }
 }
 
-export function* updatePokemonDetail(action) {
-    logger.debug('Detail from network');
-    yield put(updatePokemonDetailFetchingAction(true));
-    try {
-        const pokemon = yield call(getPokemonDetailByName, action.name);
-        yield put(updatePokemonDetailAction(pokemon));
-        yield put(updatePokemonDetailFetchingAction(false));
-    } catch (error) {
-        logger.error('Fetch updatePokemonDetail failure.', error);
-        yield put(updatePokemonDetailFetchingAction(false));
-    }
+export function* updatePokemonDetail(action: UpdatePokemonDetailProcess): Generator<any, void, any> {
+  logger.debug('Detail from network');
+  yield put(updatePokemonDetailFetchingAction(true));
+  try {
+    const pokemon = yield call(getPokemonDetailByName, action.name);
+    yield put(updatePokemonDetailAction(pokemon));
+    yield put(updatePokemonDetailFetchingAction(false));
+  } catch (error) {
+    logger.error('Fetch updatePokemonDetail failure.', error);
+    yield put(updatePokemonDetailFetchingAction(false));
+  }
 }
