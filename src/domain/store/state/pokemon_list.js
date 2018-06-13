3f3cb60c2bbc3beba61fc94a586c8b87ffe56cc4
Inatsu, Yoshinari | COED <ts-yoshinari.inatsu@rakuten.com>
2018-06-13 13:50:07 +0900

    Fix state for pokemonlistProps

--- src/domain/store/state/pokemon_list.js
+++ src/domain/store/state/pokemon_list.js
@@ -5,11 +5,11 @@ import { Record, List } from 'immutable';
 export type ItemProps = { name: string; url: string };
 export const Item = Record<ItemProps>({ name: '', url: '' });
 
-export type PokemonDetailProps = {
+export type PokemonListProps = {
   fetching: boolean,
   list: List<Item>
 }
-export const PokemonList = Record<PokemonDetailProps>({
+export const PokemonList = Record<PokemonListProps>({
   fetching: false,
   list: new List()
 });
