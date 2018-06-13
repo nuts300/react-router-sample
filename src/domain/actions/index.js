2f92a574edf379267f49a9bed6e8f327ec0ba41d
Inatsu, Yoshinari | COED <ts-yoshinari.inatsu@rakuten.com>
2018-06-13 13:58:06 +0900

    Add type for actions

--- src/domain/actions/index.js
+++ src/domain/actions/index.js
@@ -1,5 +1,6 @@
 // @flow
-import {} from 'domain/store/state';
+import type { ItemProps } from 'domain/store/state/pokemon_list';
+import type { DetailItemProps } from 'domain/store/state/pokemon_detail';
 
 export const UPDATE_POKEMON_LIST = 'UPDATE_POKEMON_LIST';
 export const UPDATE_POKEMON_DETAIL = 'UPDATE_POKEMON_DETAIL';
@@ -9,35 +10,49 @@ export const UPDATE_POKEMON_LIST_PROCESS = 'UPDATE_POKEMON_LIST_PROCESS';
 export const UPDATE_POKEMON_DETAIL_PROCESS = 'UPDATE_POKEMON_DETAIL_PROCESS';
 
 type UpdatePokemonListAction = {
-    type: typeof UPDATE_POKEMON_LIST,
-    list: any
+  type: typeof UPDATE_POKEMON_LIST,
+  list: Array<ItemProps>
 };
 
-export function updatePokemonListAction(list: any): UpdatePokemonListAction {
-    return {
-        type: UPDATE_POKEMON_LIST,
-        list: list
-    };
+type UpdatePokemonDetailAction = {
+  type: typeof UPDATE_POKEMON_DETAIL,
+  pokemon: DetailItemProps
+};
+
+type UpdatePokemonListFetchingAction = {
+  type: typeof UPDATE_POKEMON_LIST_FETCHING,
+  isFetching: boolean
+};
+
+type UpdatePokemonDetailFetchingAction = {
+  type: typeof UPDATE_POKEMON_DETAIL_FETCHING,
+  isFetching: boolean
+};
+
+export function updatePokemonListAction(list: Array<ItemProps>): UpdatePokemonListAction {
+  return {
+    type: UPDATE_POKEMON_LIST,
+    list: list
+  };
 }
 
-export function updatePokemonDetailAction(pokemon) {
-    return {
-        type: ACTIONS.UPDATE_POKEMON_DETAIL,
-        pokemon: pokemon
-    };
+export function updatePokemonDetailAction(pokemon: DetailItemProps): UpdatePokemonDetailAction {
+  return {
+    type: UPDATE_POKEMON_DETAIL,
+    pokemon: pokemon
+  };
 }
 
-export function updatePokemonListFetchingAction(isFetching) {
-    return {
-        type: ACTIONS.UPDATE_POKEMON_LIST_FETCHING,
-        isFetching
-    };
+export function updatePokemonListFetchingAction(isFetching: boolean): UpdatePokemonListFetchingAction {
+  return {
+    type: UPDATE_POKEMON_LIST_FETCHING,
+    isFetching
+  };
 }
 
-export function updatePokemonDetailFetchingAction(isFetching) {
-    return {
-        type: ACTIONS.UPDATE_POKEMON_DETAIL_FETCHING,
-        isFetching
-    };
+export function updatePokemonDetailFetchingAction(isFetching: boolean): UpdatePokemonDetailFetchingAction {
+  return {
+    type: UPDATE_POKEMON_DETAIL_FETCHING,
+    isFetching
+  };
 }
-
