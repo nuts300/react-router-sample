0a8f8ddf0239ac42cfb3c1a08831fb161787a63f
Inatsu, Yoshinari | COED <ts-yoshinari.inatsu@rakuten.com>
2018-06-13 14:07:37 +0900

    Add types for dispatcher

--- src/domain/middleware/dispatcher/pokemon.js
+++ src/domain/middleware/dispatcher/pokemon.js
@@ -1,15 +1,11 @@
+// @flow
+import { updatePokemonListProcess, updatepokemonDetailProcess } from 'domain/actions';
 import { dispatch } from 'domain/store';
-import { ACTIONS } from 'domain/actions';
 
-export function dispatchUpdatePokemonList() {
-    dispatch({
-        type: ACTIONS.UPDATE_POKEMON_LIST_PROCESS
-    });
+export function dispatchUpdatePokemonList(): void {
+  dispatch(updatePokemonListProcess());
 }
 
-export function dispatchUpdatePokemonDetail(name) {
-    dispatch({
-        type: ACTIONS.UPDATE_POKEMON_DETAIL_PROCESS,
-        name
-    });
+export function dispatchUpdatePokemonDetail(name: string): void {
+  dispatch(updatepokemonDetailProcess(name));
 }
