db080cb979127c087f2aa551d46bd4c79bebf9f2
Inatsu, Yoshinari | COED <ts-yoshinari.inatsu@rakuten.com>
2018-06-14 14:58:26 +0900

    Fix bugs

--- src/domain/middleware/index.js
+++ src/domain/middleware/index.js
@@ -1,11 +1,11 @@
-import { ACTIONS } from 'domain/actions';
+import { UPDATE_POKEMON_LIST_PROCESS, UPDATE_POKEMON_DETAIL_PROCESS } from 'domain/actions';
 import { updatePokemonList, updatePokemonDetail } from 'domain/middleware/effects/pokemon';
 import { all, takeLatest } from 'redux-saga/effects';
 
 export default function* rootSaga() {
     yield all([
-        takeLatest(ACTIONS.UPDATE_POKEMON_LIST_PROCESS, updatePokemonList),
-        takeLatest(ACTIONS.UPDATE_POKEMON_DETAIL_PROCESS, updatePokemonDetail)
+        takeLatest(UPDATE_POKEMON_LIST_PROCESS, updatePokemonList),
+        takeLatest(UPDATE_POKEMON_DETAIL_PROCESS, updatePokemonDetail)
     ]);
 }
 
