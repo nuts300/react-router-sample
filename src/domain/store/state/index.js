7c5d8a145f23a9991ad8c8196744f8f48e919faf
Inatsu, Yoshinari | COED <ts-yoshinari.inatsu@rakuten.com>
2018-06-14 15:11:07 +0900

    Remove page state

--- src/domain/store/state/index.js
+++ src/domain/store/state/index.js
@@ -2,23 +2,15 @@
 import type { ItemProps } from 'domain/store/state/pokemon_list';
 import type { DetailItemProps } from 'domain/store/state/pokemon_detail';
 
-import { Record } from 'immutable';
 import { PokemonList } from 'domain/store/state/pokemon_list';
 import { PokemonDetail } from 'domain/store/state/pokemon_detail';
 
-export type HomePage = { name: 'HOME_PAGE' };
-export type DetailPage = { name: 'DETAIL_PAGE' };
-export type PageProps = HomePage | DetailPage;
-export const Page = Record({ name: 'HOME_PAGE' });
-
 export type State = {
-  currentPage: Page,
   pokemonList: PokemonList,
   pokemonDetail: PokemonDetail
 };
 
 export type StateProps = {
-  currentPage: PageProps,
   pokemonList: {
     fetching: boolean,
     list: Array<ItemProps>
@@ -30,7 +22,6 @@ export type StateProps = {
 };
 
 export const state = {
-  currentPage: Page(),
   pokemonList: PokemonList(),
   pokemonDetail: PokemonDetail(),
 }
